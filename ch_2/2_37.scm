9a0b8916ee658dbd748b449fc1baddcc4aa9cac9
abishr-ai <abishr@aaris.ai>
2026-05-20 21:43:28 -0700

    37 mod

--- ch_2/2_37.scm
+++ ch_2/2_37.scm
@@ -15,3 +15,13 @@
 (map (lambda (x) (dot-product x v)) m)
   )
 
+(define (transpose mat)
+    (accumulate-n cons '()  mat)
+  )
+
+(define (matrix-*-matrix m n)
+  (let ((cols (transpose n)))
+    (map (lambda (row)
+           (map (lambda (col) (dot-product row col)) cols))
+         m)))
+
